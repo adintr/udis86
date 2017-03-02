680e7d351a5840162def583154724fb5abcdf555
adintr <webmaster@adintr.com>
2017-03-02 17:11:49 +0800

    改为连接静态的 CRT 库

---
+++ BuildVS2010/copylibs.bat
@@ -0,0 +1,5 @@
+copy /Y Build\Lib\x86\libudis86.lib ..\libs\libudis86_x86_release.lib
+copy /Y Build\Lib\x64\libudis86.lib ..\libs\libudis86_x64_release.lib
+copy /Y Build\Lib\Debug\x86\libudis86.lib ..\libs\libudis86_x86_debug.lib
+copy /Y Build\Lib\Debug\x64\libudis86.lib ..\libs\libudis86_x64_debug.lib
+pause
